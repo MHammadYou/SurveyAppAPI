79e336d2e368b95875ca1db0db7e06267d86f518
MHammadYou <MHammadY2.0@gmail.com>
2021-03-25 23:41:14 +0500

    Users get route now returns All users

--- users/users_routes.go
+++ users/users_routes.go
@@ -1,15 +1,40 @@
 package users
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"log"
 )
 
+type Signup struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
 func HandleUserRoutes(db *gorm.DB, r *gin.Engine) {
 
-	r.GET("/users", handleGet)
-	r.POST("/users", handlePost)
+	r.GET("/users", func (c *gin.Context) {
+
+		var users []User
+
+		db.Create(&User{Email: "jd@testing.com", Password: "testing1"})
+
+		result := db.Find(&users)
+		fmt.Println(result)
+
+		c.JSON(200, gin.H{
+			"message": users,
+		})
+	})
+
+	r.POST("/users", func (c *gin.Context) {
+		var signup Signup
+		_ = c.Bind(&signup)
+		c.JSON(200, gin.H {
+			"message": signup,
+		})
+	})
 
 	log.Fatal(r.Run(":8000"))
 }
