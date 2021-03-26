13fcf9280b5c620500c0ba788b18d02a9a846f1a
MHammadYou <MHammadY2.0@gmail.com>
2021-03-26 23:29:51 +0500

    Added User Routes

--- users/users_routes.go
+++ users/users_routes.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Signup struct {
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Email string
+	Password string
 }
 
 func HandleUserRoutes(db *gorm.DB, r *gin.Engine) {
@@ -17,8 +17,6 @@ func HandleUserRoutes(db *gorm.DB, r *gin.Engine) {
 	r.GET("/users", func (c *gin.Context) {
 
 		var users []User
-
-		db.Create(&User{Email: "jd@testing.com", Password: "testing1"})
 
 		result := db.Find(&users)
 		fmt.Println(result)
@@ -28,11 +26,62 @@ func HandleUserRoutes(db *gorm.DB, r *gin.Engine) {
 		})
 	})
 
+	r.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var user User
+		db.First(&user, id)
+
+		if user.ID != 0 {
+			c.JSON(200, gin.H {
+				"id": user,
+			})
+		} else {
+			c.JSON(404, gin.H {
+				"message": "No such user",
+			})
+		}
+
+	})
+
 	r.POST("/users", func (c *gin.Context) {
-		var signup Signup
-		_ = c.Bind(&signup)
+
+		var newUser Signup
+		_ = c.Bind(&newUser)
+
+		db.Create(&User{Email: newUser.Email, Password: newUser.Password})
+
 		c.JSON(200, gin.H {
-			"message": signup,
+			"message": "User created for " + newUser.Email,
+		})
+	})
+
+	r.DELETE("/users/:id", func (c *gin.Context) {
+		var user User
+
+		id := c.Param("id")
+
+		db.First(&user, id)
+		db.Delete(&user)
+
+		c.JSON(200, gin.H {
+			"message": "User Deleted",
+		})
+	})
+
+	r.PUT("/users/:id", func (c *gin.Context) {
+		var user User
+
+		id := c.Param("id")
+
+		var newUser Signup
+		_ = c.Bind(&newUser)
+
+		db.First(&user, id)
+		db.Model(&user).Updates(User{Email: newUser.Email, Password: newUser.Password})
+
+		c.JSON(200, gin.H {
+			"message": "Updated",
 		})
 	})
 
